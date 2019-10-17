ea4448b586a8314e43acd474414b60b821265e83
Presley <presley_wg@yahoo.com>
2019-10-17 16:52:22 +0800

    add classes

--- color-detection.py
+++ color-detection.py
@@ -1,30 +1,8 @@
 import cv2
-from toolbelt import *
+import numpy as np
+from constants import *
 
-
-glb_cam_width = 640
-glb_cam_height = 480
-
-canvas_img = np.zeros((glb_cam_width, glb_cam_height, 3), np.uint8)
-
-clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
-glb_display_img = None
-glb_insp_img = None
-glb_gray_img = None
-glb_hsv_img = None
-
-glb_roi_tl = (0,0)
-glb_roi_br = (0,0)
-glb_teach_flag = False
-glb_h_chan_max = 0
-glb_h_chan_min = 0
-glb_s_chan_max = 0
-glb_s_chan_min = 0
-glb_v_chan_max = 0
-glb_v_chan_min = 0
-
-
-class CRect:
+class CRect(object):
     def __init__(self, tL, bR):  #tL and bR should be passed as tuples
         self.tL = tL
         self.tR = (bR[0], tL[1])
@@ -73,81 +51,91 @@ class CRect:
             self.bR = (self.right, self.bR[1])
 
 
-def get_patch_hsv_range(img, rect_roi):
-    hsv_img = img.copy()
-    hsv_img = cv2.medianBlur(hsv_img, 5)
-    hsv_img = get_crop_img(hsv_img, rect_roi.tL[0], rect_roi.tL[1], rect_roi.height, rect_roi.width)
-    h_chan, s_chan, v_chan = cv2.split(hsv_img)
-    min_max_list = []
-    hsv_channel = [h_chan, s_chan, v_chan]
-    for n, chan in enumerate(hsv_channel):
-        # c_list = chan[np.nonzero(chan)]
-        min_max_list.append(np.max(chan))
-        min_max_list.append(np.min(chan))
-    return min_max_list
+class CColorDetector(object):
+    def __init__(self):
+        self.teach_flag = False
+        self.h_max = None
+        self.h_min = None
+        self.s_max = None
+        self.s_min = None
+        self.v_max = None
+        self.v_min = None
 
+    def __get_crop_img(self, src, x, y, h, w, offset=0):
+        output = src[max(0, y - offset):min(y + h + (2 * offset), src.shape[0] - 1),
+                 max(0, x - offset):min(x + w + (2 * offset), src.shape[1] - 1)]
+        return output
 
-def run_teach():
-    global glb_teach_flag, glb_h_chan_max, glb_h_chan_min, glb_s_chan_max, glb_s_chan_min, glb_v_chan_max, glb_v_chan_min
+    def __get_patch_hsv_range(self, img, rect_roi):
+        hsv_img = img.copy()
+        hsv_img = cv2.medianBlur(hsv_img, 5)
+        hsv_img = self.__get_crop_img(hsv_img, rect_roi.tL[0], rect_roi.tL[1], rect_roi.height, rect_roi.width)
+        h_chan, s_chan, v_chan = cv2.split(hsv_img)
+        min_max_list = []
+        hsv_channel = [h_chan, s_chan, v_chan]
+        for n, chan in enumerate(hsv_channel):
+            min_max_list.append(np.max(chan))
+            min_max_list.append(np.min(chan))
+        return min_max_list
 
-    teach_roi()
-    rect_roi = CRect(glb_roi_tl, glb_roi_br)
-    glb_h_chan_max, glb_h_chan_min, glb_s_chan_max, glb_s_chan_min, glb_v_chan_max, glb_v_chan_min = get_patch_hsv_range(glb_hsv_img, rect_roi)
-    glb_teach_flag = True
-
-
-def teach_roi():
-    global canvas_img, glb_teach_flag, glb_roi_tl, glb_roi_br
-
-    str = "Select ROI"
-    canvas_img = glb_insp_img.copy()
-    cv2.putText(canvas_img, "Draw a rectangle", (5, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, GREEN, 1, cv2.LINE_AA)
-    cv2.putText(canvas_img, "Press 'Enter' once done", (5, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, GREEN, 1, cv2.LINE_AA)
-    cv2.namedWindow(str, cv2.WINDOW_NORMAL)
-    x,y,w,h = cv2.selectROI(str, canvas_img, False)
-    try:
-        glb_teach_flag = True
-        glb_roi_tl = (x, y)
-        glb_roi_br = (x+w, y+h)
+    def teach_color(self, insp_img):
+        str = "Select ROI"
+        canvas_img = insp_img.copy()
+        hsv_img = cv2.cvtColor(insp_img, cv2.COLOR_BGR2HSV)
+        cv2.putText(canvas_img, "Draw a rectangle inside target object", (5, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, GREEN, 1, cv2.LINE_AA)
+        cv2.putText(canvas_img, "Press 'Enter' once done", (5, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, GREEN, 1,
+                    cv2.LINE_AA)
+        cv2.namedWindow(str, cv2.WINDOW_NORMAL)
+        x, y, w, h = cv2.selectROI(str, canvas_img, False)
+        self.teach_flag = True
+        roi_tl = (x, y)
+        roi_br = (x + w, y + h)
+        rect_roi = CRect(roi_tl, roi_br)
+        self.h_max, self.h_min, \
+        self.s_max, self.s_min, \
+        self.v_max, self.v_min = self.__get_patch_hsv_range(hsv_img, rect_roi)
         cv2.destroyWindow(str)
-    except:
-        cv2.destroyWindow(str)
 
+    def detect_color(self, insp_img):
+        hsv_img = cv2.cvtColor(insp_img, cv2.COLOR_BGR2HSV)
+        lower_color = np.array([self.h_min, self.s_min, self.v_min])
+        upper_color = np.array([self.h_max, self.s_max, self.v_max])
+        mask = cv2.inRange(hsv_img, lower_color, upper_color)
+        output = insp_img.copy()
+        output[np.where(mask == 0)] = 0
+        return output
 
-def main():
-    global glb_display_img, glb_insp_img, glb_gray_img, glb_hsv_img
+class CameraFeed(object):
+    def start(self):
+        detector = CColorDetector()
+        vidcap = cv2.VideoCapture(0)
+        main_str = "Main View"
 
-    vidcap = cv2.VideoCapture(0)
-    vidcap.set(cv2.CAP_PROP_FRAME_WIDTH, glb_cam_width)
-    vidcap.set(cv2.CAP_PROP_FRAME_HEIGHT, glb_cam_height)
-    main_str = "Main View"
+        while vidcap.isOpened():
+            success, org_img = vidcap.read()
+            k = cv2.waitKey(20)
+            display_img = org_img.copy()
+            insp_img = org_img.copy()
 
-    while vidcap.isOpened():
-        success, org_img = vidcap.read()
-        k = cv2.waitKey(20)
-        glb_display_img = org_img.copy()
-        glb_insp_img = org_img.copy()
-        glb_gray_img = cv2.cvtColor(glb_insp_img, cv2.COLOR_BGR2GRAY)
-        glb_hsv_img = cv2.cvtColor(glb_insp_img, cv2.COLOR_BGR2HSV)
+            if k == ord('t'):
+                detector.teach_color(insp_img)
 
-        if k == ord('t'):
-            run_teach()
+            if detector.teach_flag is True:
+                output_img = detector.detect_color(insp_img)
+                self.__imshow("Result View", output_img)
 
-        if glb_teach_flag:
-            lower_color = np.array([glb_h_chan_min, glb_s_chan_min, glb_v_chan_min])
-            upper_color = np.array([glb_h_chan_max, glb_s_chan_max, glb_v_chan_max])
-            mask = cv2.inRange(glb_hsv_img.copy(), lower_color, upper_color)
-            output_img = glb_insp_img.copy()
-            output_img[np.where(mask == 0)] = 0
-            output_img[np.where(mask != 0)] = 255
-            imshow("Result View", output_img)
+            self.__imshow(main_str, display_img)
 
-        imshow(main_str, glb_display_img)
+            if k == 27:
+                vidcap.release()
+                break
 
-        if k == 27:
-            vidcap.release()
-            break
+    def __imshow(self, window_string , image_source, time=None):
+        cv2.namedWindow(window_string, cv2.WINDOW_NORMAL)
+        cv2.imshow(window_string, image_source)
+        cv2.waitKey(time) if time is not None else None
 
 
 if __name__ == '__main__':
-    main()
+    cam = CameraFeed()
+    cam.start()
